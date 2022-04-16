19a95e50fb7200906a8534cef5d4aa36d2cdfded
tuhuynh <tuhuynh1902@gmail.com>
2022-04-16 22:05:32 +0700

    updating 16 April

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,7 @@
 
         var displayTitle = $(this).parents('.selectProduct').attr('data-title');
 
-        var image = $(this).parents().parents().find('.productImg').attr('src');
+        var image = $(this).siblings('.productImg').attr('src');
 
         $('.comparePan').append(
           '<div id="' +
