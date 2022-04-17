57e50ba8f6fdf83e54809ef60dcf434ab9cd447a
tuhuynh <tuhuynh1902@gmail.com>
2022-04-17 14:23:17 +0700

    updating main.js [add to Cart]

---
+++ assets/js/data.js
@@ -0,0 +1,57 @@
+var data = [
+  {
+    id: 'LG11',
+    name: 'Window Air Conditioner',
+    pic: 'LG/lg-1-1.webp',
+    price: 800,
+  },
+
+  {
+    id: 'Reetech12',
+    name: 'Reetech 1-2',
+    pic: 'Samsung/ss-2-2.png',
+    price: 850,
+  },
+
+  {
+    id: 'LG21',
+    name: 'LG 2-1',
+    pic: 'Samsung/ss-1-1.webp',
+    price: 900,
+  },
+
+  {
+    id: 'Reetech13',
+    name: 'Reetech 1-3',
+    pic: 'LG/lg-2-1.webp',
+    price: 950,
+  },
+
+  {
+    id: 'Samsung14',
+    name: 'Samsung 1-4',
+    pic: 'Samsung/ss-2-3.png',
+    price: 1000,
+  },
+
+  {
+    id: 'Samsung11',
+    name: 'Samsung 1-1',
+    pic: 'Samsung/ss-2-1.webp',
+    price: 1050,
+  },
+
+  {
+    id: 'LG31',
+    name: 'LG 3-1',
+    pic: 'LG/lg-3-1.jpg',
+    price: 1100,
+  },
+
+  {
+    id: 'Samsung21',
+    name: 'Samsung 2-1',
+    pic: 'Samsung/ss-2-4.webp',
+    price: 1150,
+  },
+];
--- assets/js/main.js
+++ assets/js/main.js
@@ -66,12 +66,12 @@
       $('.contentPop').append(
         '<div class="w3-col s3 m3 l3 relPos">' +
           '<ul class="product">' +
-          '<li class=" relPos compHeader"><p class="w3-display-middle">Features</p></li>' +
+          '<li class=" relPos compHeader"><p class="w3-display-middle">Products</p></li>' +
           '<li>Title</li>' +
-          '<li>Size</li>' +
-          '<li>Weight</li>' +
-          '<li class="cpu">Processor</li>' +
-          '<li>Battery</li></ul>' +
+          '<li>Dimension</li>' +
+          '<li>Capacity</li>' +
+          "<li>Manufacture's Brand</li>" +
+          '<li>Warranty</li></ul>' +
           '</div>'
       );
 
@@ -95,15 +95,15 @@
             title +
             '</li>' +
             '<li>' +
-            $(product).data('size') +
+            $(product).data('dimension') +
             '</li>' +
             '<li>' +
-            $(product).data('weight') +
-            '<li class="cpu">' +
-            $(product).data('processor') +
+            $(product).data('capacity') +
+            '<li>' +
+            $(product).data('manufacture') +
             '</li>' +
             '<li>' +
-            $(product).data('battery') +
+            $(product).data('warranty') +
             '</ul>' +
             '</div>'
         );
@@ -142,3 +142,192 @@
     }
   }
 })(jQuery);
+
+//Add to Cart
+var shoppingCart = (function () {
+  cart = [];
+
+  // Constructor
+  function Item(name, price, count) {
+    this.name = name;
+    this.price = price;
+    this.count = count;
+  }
+
+  // Save cart với Session Storage
+  function saveCart() {
+    sessionStorage.setItem('shoppingCart', JSON.stringify(cart));
+  }
+
+  // Load cart từ Session Storage
+  function loadCart() {
+    cart = JSON.parse(sessionStorage.getItem('shoppingCart'));
+  }
+  if (sessionStorage.getItem('shoppingCart') != null) {
+    // Ain't no given
+    loadCart();
+  }
+
+  var obj = {};
+  // Add to cart
+  obj.addItemToCart = function (name, price, count) {
+    //Bước 1: Nếu cart trùng thì ++
+    for (var i in cart) {
+      if (cart[i].name === name) {
+        cart[i].count++;
+        saveCart();
+        return;
+      }
+    }
+    //Bước 2: Nếu cart không trùng thì thêm mới vào cart[]
+    var item = new Item(name, price, count);
+    cart.push(item);
+    saveCart();
+  };
+
+  // Set count from item
+  obj.setCountForItem = function (name) {
+    for (var i in cart) {
+      if (cart[i].name === name) {
+        cart[i].count++;
+        break;
+      }
+    }
+  };
+
+  // Remove item from cart
+  obj.removeItemFromCart = function (name) {
+    for (var i in cart) {
+      if (cart[i].name === name) {
+        cart[i].count--;
+        if (cart[i].count === 0) {
+          cart.splice(i, 1);
+        }
+        break;
+      }
+    }
+    saveCart();
+  };
+
+  // Remove all items from cart
+  obj.removeItemFromCartAll = function (name) {
+    for (var i in cart) {
+      if (cart[i].name === name) {
+        cart.splice(i, 1);
+        break;
+      }
+    }
+    saveCart();
+  };
+
+  // Clear cart
+  obj.clearCart = function () {
+    cart = [];
+    saveCart();
+  };
+
+  // Count cart
+  obj.totalCount = function () {
+    var totalCount = 0;
+    for (var i in cart) {
+      totalCount += cart[i].count;
+    }
+    return totalCount;
+  };
+
+  // Total cart
+  obj.totalCart = function () {
+    var totalCart = 0;
+    for (var i in cart) {
+      totalCart += cart[i].price * cart[i].count;
+    }
+    return Number(totalCart);
+  };
+
+  // List cart
+  obj.listCart = function () {
+    var cartCopy = [];
+    for (i in cart) {
+      item = cart[i];
+      itemCopy = [];
+      for (p in item) {
+        itemCopy[p] = item[p];
+      }
+      itemCopy.total = Number(item.price * item.count);
+      cartCopy.push(itemCopy);
+    }
+    return cartCopy;
+  };
+  return obj;
+})();
+
+// Add item
+$('.add-to-cart').click(function (event) {
+  event.preventDefault();
+  var name = $(this).data('name');
+  var price = Number($(this).data('price'));
+  shoppingCart.addItemToCart(name, price, 1);
+  displayCart();
+});
+
+// Clear items
+$('.clear-cart').click(function () {
+  shoppingCart.clearCart();
+  displayCart();
+});
+
+function displayCart() {
+  var cartArray = shoppingCart.listCart();
+  var output = `<tr>
+                      <th>Name</th>
+                      <th>Price</th>
+                      <th>Quantity</th>
+                      <th> </th>
+                      <th>Total</th>
+                    </tr>`;
+  for (var i in cartArray) {
+    output += `<tr>
+                  <td>${cartArray[i].name}</td>
+                  <td>${cartArray[i].price}</td>
+                  <td><div class='input-group'><span class='minus-item input-group-addon btn btn-primary' data-name="${cartArray[i].name}">-</span>
+                      <input type='number' class='item-count form-control' data-name="${cartArray[i].name}" value="${cartArray[i].count}">
+                      <span class='plus-item input-group-addon btn btn-primary' data-name="${cartArray[i].name}">+</span></div></td> 
+                  <td><button class='delete-item btn btn-danger' data-name="${cartArray[i].name}">X</button></td>
+                  <td>${cartArray[i].total}</td>
+                  </tr>`;
+  }
+  $('.show-cart').html(output);
+  $('.total-cart').html(shoppingCart.totalCart());
+  $('.total-count').html('$' + shoppingCart.totalCart());
+}
+
+// Delete-item
+$('.show-cart').on('click', '.delete-item', function () {
+  var name = $(this).data('name');
+  alert('delete ' + name);
+  shoppingCart.removeItemFromCartAll(name);
+  displayCart();
+});
+
+// Minus-item
+$('.show-cart').on('click', '.minus-item', function () {
+  var name = $(this).data('name');
+  shoppingCart.removeItemFromCart(name);
+  displayCart();
+});
+// Plus-item
+$('.show-cart').on('click', '.plus-item', function () {
+  var name = $(this).data('name');
+  shoppingCart.addItemToCart(name);
+  displayCart();
+});
+
+// Item-icount <input>
+$('.show-cart').on('change', '.item-count', function () {
+  var name = $(this).data('name');
+  var count = Number($(this).val());
+  shoppingCart.setCountForItem(name, count);
+  displayCart();
+});
+
+displayCart();
